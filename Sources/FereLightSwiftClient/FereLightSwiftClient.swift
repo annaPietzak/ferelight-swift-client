ee7a138dad4abb1bc4214e06c8f327d4a4a4f6cf
annaPietzak <anna.pietzak@gmail.com>
2025-06-27 08:54:08 +0200

    implement extended query aoi call

--- Sources/FereLightSwiftClient/FereLightSwiftClient.swift
+++ Sources/FereLightSwiftClient/FereLightSwiftClient.swift
@@ -94,43 +94,11 @@ public struct FereLightClient : Sendable {
     ///   - limit: Maximum number of results to return.
     ///
     /// - Returns: Array of segment ID and similarity score pairs.
-    public func query(database: String, similarityText: String?, ocrText: String?, limit: Int?) async throws -> [(segmentId: String, score: Double)] {
-        let response = try await client.post_sol_query(body: .json(.init(database: database, similaritytext: similarityText, ocrtext: ocrText, limit: limit)))
+    public func query(database: String, similarityText: String?, ocrText: String?, asrtext: String?, mergetype: String?, limit: Int?) async throws -> [(segmentId: String, score: Double)] {
+        let response = try await client.post_sol_query(body: .json(.init(database: database, similaritytext: similarityText, ocrtext: ocrText, asrtext: asrtext, mergetype: mergetype, limit: limit)))
         
         let message = try response.ok.body.json
         
         return message.map { ($0.segmentid!, $0.score!) }
-    }
-    
-    /// Queries the database using the provided segment as an example.
-    ///
-    /// - Parameters:
-    ///   - database: The name of the database to query.
-    ///   - segmentId: The ID of the segment to use as example.
-    ///   - limit: Maximum number of results to return.
-    ///
-    /// - Returns: Array of segment ID and similarity score pairs.
-    public func queryByExample(database: String, segmentId: String, limit: Int?) async throws -> [(segmentId: String, score: Double)] {
-        let response = try await client.post_sol_querybyexample(body: .json(.init(database: database, segmentid: segmentId, limit: limit)))
-        
-        let message = try response.ok.body.json
-        
-        return message.map { ($0.segmentid!, $0.score!) }
-    }
-    
-    /// Retrieve the segment at the specified time within the object.
-    ///
-    /// - Parameters:
-    ///   - database: The name of the database to query.
-    ///   - objectId: The ID of the object to find the segment of.
-    ///   - timestamp: The time of the segment within the object.
-    ///
-    /// - Returns: Segment ID of the segment at the specified time within the object.
-    public func segmentByTime(database: String, objectId: String, timestamp: Double) async throws -> String {
-        let response = try await client.post_sol_segmentbytime(body: .json(.init(database: database, objectid: objectId, timestamp: timestamp)))
-        
-        let message = try response.ok.body.json
-        
-        return message.segmentid!
     }
 }
